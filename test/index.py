f599d7bf1e55f4131a47607214a7225cd45ab080
Asonibare <89985142+Asonibare@users.noreply.github.com>
2022-08-26 23:54:52 +0100

    Update index.py

--- test/index.py
+++ test/index.py
@@ -1,7 +1,7 @@
-from flask import Flask, Response
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route("/")
+def home():
+    return "<h2>Flask Vercel</h2>"
